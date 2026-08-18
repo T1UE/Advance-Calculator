1f0f9e8f036ebd4be22dfe4d12d684f6e18b78c7
T1UE <maibasanthoon@gmail.com>
2026-08-18 23:24:04 +0530

    add list animation

--- README.md
+++
@@ -1,2 +0,0 @@
-# Advance-Calculator
-An advanced calculator designed to handle complex mathematical operations with a clean and user-friendly interface.
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 from colorama import Fore
+import time
+import ctypes
+
+RED = Fore.RED
+BLUE = Fore.BLUE
+YELLOW = Fore.YELLOW
+CYAN = Fore.CYAN
+L_YELLOW = Fore.LIGHTYELLOW_EX
+WHITE = Fore.WHITE
+
+functions = ["Basic Calculations","Matrix Calculations","Advanced Algebra","Calculus","Statistics and Probability","Vector Mathematics","Graphing Engine","Number Theory","Complex Mathematics","Linear Algebra - SUPER Advanced","Geometry","Differential Equations"]
+
+for i in range(len(functions)):
+    str_fn = functions[i]
+    for j in range(len(str_fn)):
+        if(i%2 == 0):
+            print(f"{YELLOW}", end="")
+        else:
+            print(f"{L_YELLOW}", end="")
+        print(str_fn[j], end="", flush=True)
+        time.sleep(0.07)
+    print()
+
+    
+def Basic_Calculations():
+    pass
+
