201ec1d7b45692078fe27e198c31dc32832d4fe3
T1UE <maibasanthoon@gmail.com>
2026-08-27 12:55:58 +0530

    Update graphing.py

--- Graphing/graphing.py
+++ Graphing/graphing.py
@@ -18,18 +18,18 @@ def GUI_VIEW():
     y = x**2 + 3*x + 2
     matplt.plot(x, y)
     matplt.grid()
-    # matplt.show()
-    images = os.listdir("Graphing/Images")
-    print(images)
-    count = len(images)
-    image_name = input("Enter File Name to be Saved (default : image.png) ")
-    if len(image_name) == 0:
-        matplt.savefig(f"Graphing/Images/image{count+1}.png", dpi = 300)
-    else:
-        try:
-            matplt.savefig(f"Graphing/Images/{image_name}.png", dpi = 300)
-        except:
-            print("Conflict in file name / already used !")
+    matplt.show()
+    #images = os.listdir("Graphing/Images")
+    #print(images)
+    #count = len(images)
+    #image_name = input("Enter File Name to be Saved (default : image.png) ")
+    #if len(image_name) == 0:
+       # matplt.savefig(f"Graphing/Images/image{count+1}.png", dpi = 300)
+   # else:
+        #try:
+            #matplt.savefig(f"Graphing/Images/{image_name}.png", dpi = 300)
+       # except:
+            #print("Conflict in file name / already used !")
 
 
 
